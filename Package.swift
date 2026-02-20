fe5a0479a7f6ab0cb804bd5de96dada4cb6408f9
Andre Bongartz <andre.bongartz@bitsmaker.de>
2026-02-20 15:03:48 +0100

    add dependenies

--- Package.swift
+++ Package.swift
@@ -6,24 +6,27 @@ import PackageDescription
 let package = Package(
     name: "PeerToPeerMessaging",
     platforms: [
-        .iOS(.v18),
-        .visionOS(.v2),
-        .macOS(.v14)
+        .visionOS(.v26),
+        .iOS(.v26)
     ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PeerToPeerMessaging",
             targets: ["PeerToPeerMessaging"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-certificates.git", .upToNextMajor(from: "1.0.0"))
+    ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PeerToPeerMessaging",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "PeerToPeerMessagingTests",
-            dependencies: ["PeerToPeerMessaging"]
+            dependencies: [.product(name: "X509", package: "swift-certificates")],
+
         )
+
     ]
 )
