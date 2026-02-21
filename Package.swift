f433949ad99bbe1a32e67ff19f2e5682f2ab2b60
Andre Bongartz <andre.bongartz@bitsmaker.de>
2026-02-21 07:34:13 +0100

    update swiftooling version to 6.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,11 +6,11 @@ import PackageDescription
 let package = Package(
     name: "PeerToPeerMessaging",
     platforms: [
+        .iOS(.v26),
         .visionOS(.v26),
-        .iOS(.v26)
+        .macOS(.v26)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PeerToPeerMessaging",
             targets: ["PeerToPeerMessaging"]
@@ -20,13 +20,14 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-certificates.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PeerToPeerMessaging",
-            dependencies: [.product(name: "X509", package: "swift-certificates")],
-
+            dependencies: [.product(name: "X509", package: "swift-certificates")]
+        ),
+        .testTarget(
+            name: "PeerToPeerMessagingTests",
+            dependencies: ["PeerToPeerMessaging"]
         )
-
     ]
 )
+
